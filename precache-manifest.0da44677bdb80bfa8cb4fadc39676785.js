5f20c3c36eb639eb09d36378e777f65e631c3b2c
DinaSem <neispravomayas1@gmail.com>
2022-02-25 21:47:36 +0300

    Updates

---
+++ precache-manifest.0da44677bdb80bfa8cb4fadc39676785.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29653095b96f56f3800440da900ef1ab",
+    "url": "/Tickets/index.html"
+  },
+  {
+    "revision": "9f316109992ac0199942",
+    "url": "/Tickets/static/css/main.d54d6eb1.chunk.css"
+  },
+  {
+    "revision": "3d509024827dc9397903",
+    "url": "/Tickets/static/js/2.944bae65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tickets/static/js/2.944bae65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f316109992ac0199942",
+    "url": "/Tickets/static/js/main.fa30b043.chunk.js"
+  },
+  {
+    "revision": "058c4c28a48121079b5d",
+    "url": "/Tickets/static/js/runtime-main.668e554b.js"
+  }
+]);
